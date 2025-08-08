045ea17aaf8dd84de28b607b9c426185b1ff964c
Jibrael <imjibraelmalik@gmail.com>
2025-08-08 10:32:12 +0500

    Add files via upload

---
+++ fav dish.py
@@ -0,0 +1,16 @@
+Dish = input("ap ka sub se psenddeda khana kiya hai?")
+
+if Dish == "Burger":
+    print("Very good choice")
+elif Dish == "Pizza":
+    print("Perfect choice very good pet bher ne ke liye")
+elif Dish == "Dal chawal":
+    print("ok gi :|")
+elif Dish == "phiki Bread":
+    print("y choose this just kiyu its tasteless")
+else: 
+     print("Jo ammi banye gi wo hi khane ko mele ga warna to ammi ki chapal hi mele gi >>>:)))")
+
+
+
+
