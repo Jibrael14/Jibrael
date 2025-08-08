08e472a1fb227c91c77ba6703ca8dd41f28ead9f
Jibrael <imjibraelmalik@gmail.com>
2025-08-08 10:31:40 +0500

    Add files via upload

---
+++ grade.py
@@ -0,0 +1,16 @@
+marks = 10
+
+if marks >= 90:
+    print("A+")
+elif marks >= 80:
+    print("A")
+elif marks >= 70:
+    print("B")    
+elif marks >= 60:
+    print("C")
+elif marks >= 50:
+    print("D")
+elif marks >= 40:
+    print("Pass")
+else:
+    print("Fail")
